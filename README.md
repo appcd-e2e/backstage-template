66f4bf185d43969429f3e5ddd05b138757e17113
Abhishes Shukla <49184526+abhishes007@users.noreply.github.com>
2025-03-04 12:02:22 +0530

    feat: create a test backstage component

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # backstage-template
 Test repo for backstage template
+
+# My Backstage Component
+
+This is a simple Backstage component that can be registered in a Backstage instance.
+
+## Usage
+1. Clone this repository.
+2. Register `catalog-info.yaml` in Backstage.
---
+++ src/components/MyComponent.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Card, CardContent } from "@mui/material";
+
+const MyComponent = () => {
+  return (
+    <Card>
+      <CardContent>
+        <h2>My Backstage Component</h2>
+        <p>This is a simple component for Backstage.</p>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default MyComponent;
